9b96901335a665078eb4a3c3bd75835095fa9f8f
Andres Condo <1dkevinandres.b@gmail.com>
2020-07-11 19:07:03 -0500

    created service workers (now offline)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
+//console.log('Todo bien ya funciona el service workers')
 
-
-
-console.log('Todo bien')
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch(error => {
+        console.log(error.message);
+    })
+}
